ce5e2050b7ea8eebed15cb508eaff0d6e2a1dcf7
Balamurugan <dhivaab@gmail.com>
2020-10-07 09:23:26 +0530

    redux saga

---
+++ reduxsaga/reduxex/src/redux/store.js
@@ -0,0 +1,14 @@
+import rootReducer from './reducers/index.js';
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './saga/index';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = compose(
+    applyMiddleware(sagaMiddleware)
+)(createStore)(rootReducer);
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
